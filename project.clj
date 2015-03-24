b3b43e11732962189b8a674c9b9358e1364b7457
Sheel Choksi <sheelchoksi@gmail.com>
2015-03-24 16:27:58 -0700

    Whitespace fix

--- project.clj
+++ project.clj
@@ -5,9 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[s3-wagon-private "1.1.2"]]
   :repositories [["releases" {:url "s3p://private-bucket-name/releases/"
-                             :username :env
-                             :passphrase :env
-                             :sign-releases false}]]
+                              :username :env
+                              :passphrase :env
+                              :sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring/ring-jetty-adapter "1.3.2"]]
   :profiles
