146dd2d4191091cb700e3f1f9389737539de9c5d
Sheel Choksi <sheelchoksi@gmail.com>
2015-06-05 16:02:23 -0700

    Remove s3-wagon-private

--- project.clj
+++ project.clj
@@ -4,12 +4,7 @@
   :url "https://github.com/Mayvenn/standalone-test-server"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[s3-wagon-private "1.1.2"]
-            [codox "0.8.12"]]
-  :repositories [["releases" {:url "s3p://private-bucket-name/releases/"
-                              :username :env
-                              :passphrase :env
-                              :sign-releases false}]]
+  :plugins [[codox "0.8.12"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring/ring-jetty-adapter "1.3.2"]]
   :codox {:include standalone-test-server.core
