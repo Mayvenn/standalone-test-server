11cf9d88104c0fa6e1ace00daac574ca4b4dbb78
Jeff Hui <jeff@mayvenn.com>
2015-08-07 15:01:26 -0700

    Version 0.4.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject standalone-test-server "0.4.0"
+(defproject standalone-test-server "0.4.1-SNAPSHOT"
   :description "An in-process server that can record requests.
                Useful for testing code that makes external http requests"
   :url "https://github.com/Mayvenn/standalone-test-server"
