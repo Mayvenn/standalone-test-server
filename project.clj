14e1caab5ac4b5bbd5933e2b39696b79695f7788
Jacob Maine <jacob@mayvenn.com>
2017-01-14 16:36:05 -0800

    Version 0.6.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject standalone-test-server "0.6.0"
+(defproject standalone-test-server "0.6.1-SNAPSHOT"
   :description "An in-process server that can record requests.
                Useful for testing code that makes external http requests"
   :url "https://github.com/Mayvenn/standalone-test-server"
